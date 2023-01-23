b2a4eb201a7a0708140a70fcd796b405b2f868e7
Marcos Pinheiro <pinheiro.tech@gmail.com>
2023-01-23 15:59:13 -0300

    Set initial IaC with Terraform

--- week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
+++ week_1_basics_n_setup/1_terraform_gcp/terraform/variables.tf
@@ -3,22 +3,29 @@ locals {
 }
 
 variable "project" {
-  description = "Your GCP Project ID"
+  description = "zoomcamp-374917"
+
 }
 
 variable "region" {
-  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
-  default = "europe-west6"
-  type = string
+  description = "Region for GCP resources."
+  default     = "us-central1"
+  type        = string
 }
 
 variable "storage_class" {
   description = "Storage class type for your bucket. Check official docs for more info."
-  default = "STANDARD"
+  default     = "STANDARD"
 }
 
 variable "BQ_DATASET" {
   description = "BigQuery Dataset that raw data (from GCS) will be written to"
-  type = string
-  default = "trips_data_all"
+  type        = string
+  default     = "trips_data_all"
+}
+
+variable "TABLE_NAME" {
+  description = "Biguery table"
+  type        = string
+  default     = "ny_trips"
 }
